91133403ddae9ec22464d628d146aee5acfbd6b9
nullawhale <kura98.21@gmail.com>
2020-01-27 14:09:21 +0300

    test Саша

--- bot.py
+++ bot.py
@@ -3,33 +3,30 @@ import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 def start(update, context):
-    """Send a message when the command /start is issued."""
     update.message.reply_text('Hello.')
 
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
 def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    if update.message.text == 'Саша':
+        update.message.reply_text('Александр')
 
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
-    """Start the bot."""
     token = os.environ['TELEGRAM_TOKEN']
     updater = Updater(token, use_context=True)
 
